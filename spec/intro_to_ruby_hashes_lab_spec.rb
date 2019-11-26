294fff6ce3185239fc5c09a430456043e627dcaa
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-26 03:25:37 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -51,8 +51,8 @@ end
 describe "reading data from a book" do
   describe "read_from_book" do
     it "returns the value corresponding to the provided key" do
-      expect(read_from_hash({name: 'Steve'}, :name)).to eq('Steve')
-      expect(read_from_hash({'name' => 'Tzvi'}, 'name')).to eq('Tzvi')
+      expect(read_from_hash({name: 'Steve'}, :book)).to eq('Steve')
+      expect(read_from_hash({'name' => 'Tzvi'}, 'book')).to eq('Tzvi')
     end
 
     it "returns nil if the key is not found in the provided hash" do
